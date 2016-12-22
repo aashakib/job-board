5294f98df309dc74f8d8c7c23c955e3bf9213a55
Abdullah Al Shakib <aashakib@yahoo.com>
2016-12-23 01:22:47 +0600

    application layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<script src="//cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Board</title>
+    <link rel="stylesheet" media="all" type="text/css" href="//cdn.jsdelivr.net/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
