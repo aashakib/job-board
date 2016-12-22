81ee251bd86187749a43bbea2df5fd49bf63b249
Abdullah Al Shakib <aashakib@yahoo.com>
2016-12-23 01:09:56 +0600

    db migration files

---
+++ database/migrations/2016_12_22_190251_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('email')->unique();
+            $table->tinyInteger('status')->default(0)->comment('0=pending, 1=public/published, 2=spam');
+            $table->integer('user_id');
+            $table->integer('approved_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
