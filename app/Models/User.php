fc38c0096ea495da3c0835a1fe229b30b98296b2
Abdullah Al Shakib <aashakib@yahoo.com>
2016-12-31 01:02:42 +0600

    add scope for getting admin user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function scopeAdminUser($query)
+    {
+        return $query->where('type', 1);
+    }
 }
